f31130a8612f13b0717bf5501b65cb7eb2742b36
Yash Bhatt <0yashbhatt0@gmail.com>
2024-10-29 23:47:13 +0530

    add about section

---
+++ src/components/about/elements/Info.jsx
@@ -0,0 +1,30 @@
+
+export default function Info() {
+  return (
+    <div className="about__info grid">
+        <div className="about__box">
+            <i className="bx bx-award about__icon"></i>
+            <h3 className="about__title">Experience</h3>
+                <span className="about__subtitle">6 Months</span>
+            
+        </div>
+
+        
+        <div className="about__box">
+            <i className="bx bx-briefcase-alt about__icon"></i>
+            <h3 className="about__title">Projects</h3>
+                <span className="about__subtitle">20+</span>
+            
+        </div>
+        
+        <div className="about__box">
+        <i className='bx bx-book-open about__icon'></i>
+            <h3 className="about__title">Education</h3>
+                <span className="about__subtitle">Bachelor's</span>
+            
+        </div>
+
+
+    </div>
+  )
+}
